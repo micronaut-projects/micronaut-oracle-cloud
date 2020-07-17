412ea3c6e3c7b0e75c3170d4e16067e0a43bad58
graemerocher <graeme.rocher@gmail.com>
2020-07-17 09:18:51 +0200

    Cleanup

--- examples/example-java-function/src/main/java/example/ListBucketsFunction.java
+++ examples/example-java-function/src/main/java/example/ListBucketsFunction.java
@@ -11,7 +11,6 @@ import io.micronaut.oci.function.OciFunction;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,23 +24,16 @@ public class ListBucketsFunction extends OciFunction {
 
     @ReflectiveAccess
     public List<String> handleRequest() {
-        try {
-            String tenancyId = tenantIdProvider.getTenancyId();
+        String tenancyId = tenantIdProvider.getTenancyId();
 
-            GetNamespaceRequest getNamespaceRequest = GetNamespaceRequest.builder()
-                .compartmentId(tenancyId).build();
-            String namespace = objectStorageClient.getNamespace(getNamespaceRequest).getValue();
-            final ListBucketsRequest.Builder builder = ListBucketsRequest.builder();
-            builder.namespaceName(namespace);
-            builder.compartmentId(tenancyId);
-            return objectStorageClient.listBuckets(builder.build())
-                    .getItems().stream().map(BucketSummary::getName)
-                    .collect(Collectors.toList());
-        } catch (Throwable e) {
-            System.out.println("Error occurred: " + e.getMessage());
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-
+        GetNamespaceRequest getNamespaceRequest = GetNamespaceRequest.builder()
+            .compartmentId(tenancyId).build();
+        String namespace = objectStorageClient.getNamespace(getNamespaceRequest).getValue();
+        final ListBucketsRequest.Builder builder = ListBucketsRequest.builder();
+        builder.namespaceName(namespace);
+        builder.compartmentId(tenancyId);
+        return objectStorageClient.listBuckets(builder.build())
+                .getItems().stream().map(BucketSummary::getName)
+                .collect(Collectors.toList());
     }
 }
