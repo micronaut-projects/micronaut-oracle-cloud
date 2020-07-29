6dbce324df0dc811f5e4de316402cef0ae1dbba2
graemerocher <graeme.rocher@gmail.com>
2020-07-29 17:50:22 +0200

    Document how to get hold of current region

--- oci-common/src/test/java/io/micronaut/oci/core/OracleCloudCoreDefaultConfigTest.java
+++ oci-common/src/test/java/io/micronaut/oci/core/OracleCloudCoreDefaultConfigTest.java
@@ -2,6 +2,7 @@ package io.micronaut.oci.core;
 
 import com.oracle.bmc.ClientConfiguration;
 import com.oracle.bmc.auth.AuthenticationDetailsProvider;
+import com.oracle.bmc.auth.RegionProvider;
 import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +18,11 @@ public class OracleCloudCoreDefaultConfigTest {
     @Test
     void testDefaultConfig(
             ClientConfiguration clientConfiguration,
-            AuthenticationDetailsProvider provider) {
+            AuthenticationDetailsProvider provider,
+            RegionProvider regionProvider) {
         assertEquals(60000, clientConfiguration.getReadTimeoutMillis());
         assertNotNull(clientConfiguration);
         assertNotNull(provider);
+        assertNotNull(regionProvider);
     }
 }
