99e5fcc509f529d4683c672c7fc28f7f4a1acf8a
graemerocher <graeme.rocher@gmail.com>
2021-01-19 13:12:49 +0100

    fix checkstyle

--- oraclecloud-sdk/src/main/java/io/micronaut/oraclecloud/clients/SdkClients.java
+++ oraclecloud-sdk/src/main/java/io/micronaut/oraclecloud/clients/SdkClients.java
@@ -18,6 +18,9 @@ package io.micronaut.oraclecloud.clients;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Internal Annotation to trigger the creation of SDK clients.
+ */
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface SdkClients {
     Class<?>[] value();
