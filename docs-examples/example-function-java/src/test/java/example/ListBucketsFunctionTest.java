ef86cf7b0d6dbe1b03fa41dd3d391c205cd268f4
graemerocher <graeme.rocher@gmail.com>
2021-01-20 12:24:37 +0100

    Disable test on Java 8

--- docs-examples/example-function-java/src/test/java/example/ListBucketsFunctionTest.java
+++ docs-examples/example-function-java/src/test/java/example/ListBucketsFunctionTest.java
@@ -3,10 +3,13 @@ package example;
 import com.fnproject.fn.testing.FnTestingRule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 public class ListBucketsFunctionTest {
 
     @Test
+    @DisabledOnJre(value=JRE.JAVA_8, disabledReason = "Possible bug in FDK means this requires Java 11")
     void testFunction() {
         FnTestingRule fn = newFnRule();
         fn.givenEvent().enqueue();
