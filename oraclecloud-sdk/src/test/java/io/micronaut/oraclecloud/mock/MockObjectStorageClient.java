0ca08f2e7463a63534ac96c8077836fd091c4202
Nemanja Mikic <nemanja.mikic@oracle.com>
2022-08-25 10:40:26 +0200

    Fix tests

---
+++ oraclecloud-sdk/src/test/java/io/micronaut/oraclecloud/mock/MockObjectStorageClient.java
@@ -0,0 +1,31 @@
+package io.micronaut.oraclecloud.mock;
+
+import com.oracle.bmc.auth.BasicAuthenticationDetailsProvider;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+import com.oracle.bmc.objectstorage.model.BucketSummary;
+import com.oracle.bmc.objectstorage.requests.ListBucketsRequest;
+import com.oracle.bmc.objectstorage.responses.ListBucketsResponse;
+import io.micronaut.context.annotation.Replaces;
+
+import jakarta.inject.Singleton;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Singleton
+@Replaces(ObjectStorageClient.class)
+public class MockObjectStorageClient extends ObjectStorageClient {
+
+    public MockObjectStorageClient(BasicAuthenticationDetailsProvider authDetailsProvider) {
+        super(authDetailsProvider);
+    }
+
+    @Override
+    public ListBucketsResponse listBuckets(ListBucketsRequest request) {
+        List<BucketSummary> bucketSummaries = MockData.bucketNames.stream()
+                .map(name -> BucketSummary.builder().name(name).build())
+                .collect(Collectors.toList());
+        return ListBucketsResponse.builder()
+                .items(bucketSummaries)
+                .build();
+    }
+}
---
+++ oraclecloud-sdk/src/test/java/io/micronaut/oraclecloud/objectstorage/ObjectStorageClientTest.java
@@ -0,0 +1,88 @@
+package io.micronaut.oraclecloud.objectstorage;
+
+import com.oracle.bmc.objectstorage.ObjectStorageAsync;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+import com.oracle.bmc.objectstorage.model.BucketSummary;
+import com.oracle.bmc.objectstorage.requests.ListBucketsRequest;
+import com.oracle.bmc.objectstorage.responses.ListBucketsResponse;
+import com.oracle.bmc.responses.AsyncHandler;
+import io.micronaut.oraclecloud.core.TenancyIdProvider;
+import io.micronaut.oraclecloud.mock.MockData;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+public class ObjectStorageClientTest {
+
+    private final ObjectStorageClient objectStorageClient;
+    private final TenancyIdProvider tenancyIdProvider;
+    private final ObjectStorageAsync objectStorageAsync;
+
+    public ObjectStorageClientTest(
+            ObjectStorageClient objectStorageClient,
+            ObjectStorageAsync objectStorageAsync,
+            TenancyIdProvider tenancyIdProvider) {
+        this.objectStorageClient = objectStorageClient;
+        this.tenancyIdProvider = tenancyIdProvider;
+        this.objectStorageAsync = objectStorageAsync;
+    }
+
+    @Test
+    void testObjectStorageClient() {
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder()
+                .namespaceName("kg")
+                .compartmentId(tenancyIdProvider.getTenancyId())
+                .build();
+        final List<BucketSummary> items = objectStorageClient
+                .listBuckets(listBucketsRequest)
+                .getItems();
+
+        List<String> names = items.stream().map(BucketSummary::getName).collect(Collectors.toList());
+        assertEquals(Arrays.asList("b1", "b2"), names);
+    }
+
+    @Test
+    void testObjectStorageAsync() throws ExecutionException, InterruptedException {
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder()
+                .namespaceName("kg")
+                .compartmentId(tenancyIdProvider.getTenancyId())
+                .build();
+        CompletableFuture<List<BucketSummary>> future = new CompletableFuture<>();
+        objectStorageAsync
+                .listBuckets(listBucketsRequest, new AsyncHandler<ListBucketsRequest, ListBucketsResponse>() {
+                    @Override
+                    public void onSuccess(ListBucketsRequest listBucketsRequest, ListBucketsResponse listBucketsResponse) {
+                        future.complete(listBucketsResponse.getItems());
+                    }
+
+                    @Override
+                    public void onError(ListBucketsRequest listBucketsRequest, Throwable error) {
+                        future.completeExceptionally(error);
+                    }
+                });
+
+        assertNotNull(future.get());
+    }
+
+    @BeforeEach
+    void setup() {
+        MockData.bucketNames.add("b1");
+        MockData.bucketNames.add("b2");
+    }
+
+    @AfterEach
+    void cleanup() {
+        MockData.reset();
+    }
+}
