270ce21b544cbd9341071f3d013dacce79e9241b
yawkat <jonas.konrad@oracle.com>
2022-11-11 14:23:01 +0100

    test

--- oraclecloud-httpclient-netty/src/test/java/io/micronaut/oraclecloud/httpclient/netty/BufferFutureHandlerTest.java
+++ oraclecloud-httpclient-netty/src/test/java/io/micronaut/oraclecloud/httpclient/netty/BufferFutureHandlerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
 class BufferFutureHandlerTest {
@@ -71,6 +72,26 @@ class BufferFutureHandlerTest {
             Assertions.fail();
         } catch (RuntimeException e) {
             Assertions.assertEquals("bar", e.getMessage());
+        }
+    }
+
+    @Test
+    public void cancel() throws Exception {
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel();
+        BufferFutureHandler handler = new BufferFutureHandler();
+        embeddedChannel.pipeline().addLast(handler);
+
+        Assertions.assertFalse(handler.future.isDone());
+        embeddedChannel.writeInbound(Unpooled.wrappedBuffer("foo".getBytes(StandardCharsets.UTF_8)));
+        Assertions.assertFalse(handler.future.isDone());
+        embeddedChannel.pipeline().remove(handler);
+        Assertions.assertTrue(handler.future.isDone());
+
+        try {
+            handler.future.get();
+            Assertions.fail();
+        } catch (CancellationException e) {
+            // should happen
         }
     }
 }
