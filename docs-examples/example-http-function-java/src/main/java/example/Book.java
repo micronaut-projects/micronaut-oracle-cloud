16bbc8d1a04b73ed463f6470057c34ff9780c8b6
Dean Wette <wetted@objectcomputing.com>
2023-03-15 14:45:33 -0500

    build: plugin 6.3.5, gradle 8.0.2, jakarta validation, standardized project names, cleanup

--- docs-examples/example-http-function-java/src/main/java/example/Book.java
+++ docs-examples/example-http-function-java/src/main/java/example/Book.java
@@ -17,9 +17,9 @@ package example;
 
 import io.micronaut.core.annotation.Introspected;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Introspected
 public class Book {
