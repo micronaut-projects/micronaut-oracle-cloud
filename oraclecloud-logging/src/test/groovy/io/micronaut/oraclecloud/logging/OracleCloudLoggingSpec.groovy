d5e486e1cde3766c11fd300c6ff23108c3ea4c4e
Graeme Rocher <graeme.rocher@oracle.com>
2023-05-17 11:51:00 +0200

    fix build

--- oraclecloud-logging/src/test/groovy/io/micronaut/oraclecloud/logging/OracleCloudLoggingSpec.groovy
+++ oraclecloud-logging/src/test/groovy/io/micronaut/oraclecloud/logging/OracleCloudLoggingSpec.groovy
@@ -148,11 +148,6 @@ class OracleCloudLoggingSpec extends Specification {
 
         }
 
-        @Override
-        void useRealmSpecificEndpointTemplate(boolean b) {
-
-        }
-
         List<PutLogsRequest> getPutLogsRequestList() {
             synchronized (putLogsRequestList) {
                 return new ArrayList<PutLogsRequest>(putLogsRequestList)
