f1f9cf748bc6d7bd2e5bd5df566a3c7c3e19bd93
Graeme Rocher <graeme.rocher@oracle.com>
2023-05-23 15:23:07 +0200

    only run test on Oracle Cloud CI

--- oraclecloud-vault/src/test/groovy/io/micronaut/oraclecloud/discovery/vault/OracleCloudVaultConfigurationClientNoConfigSpec.groovy
+++ oraclecloud-vault/src/test/groovy/io/micronaut/oraclecloud/discovery/vault/OracleCloudVaultConfigurationClientNoConfigSpec.groovy
@@ -8,11 +8,13 @@ import io.micronaut.context.annotation.Property
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
+import spock.lang.Requires
 import spock.lang.Specification
 
 @MicronautTest(contextBuilder = MyContextBuilder)
 @Property(name = "micronaut.config-client.enabled", value = "true")
 @Property(name = "oci.vault.config.enabled", value = "true")
+@Requires({ System.getenv("VAULT_OCID") && System.getenv("VAULT_SECRETS_COMPARTMENT_OCID") && System.getenv("VAULT_SECRET_NAME") && System.getenv("VAULT_SECRET_VALUE") })
 class OracleCloudVaultConfigurationClientNoConfigSpec extends Specification {
     @Inject ApplicationContext context
     void "context still starts"() {
