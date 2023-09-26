dd4eb9d526ffd6997c8718e60cc39f2324f55d61
Graeme Rocher <graeme.rocher@oracle.com>
2023-09-26 15:54:57 +0200

    fix broken serialization/deserialization

---
+++ test-suite-java/src/test/java/example/VaultSerializationTest.java
@@ -0,0 +1,29 @@
+package example;
+
+import com.oracle.bmc.vault.model.SecretSummary;
+import io.micronaut.json.JsonMapper;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+public class VaultSerializationTest {
+    @Test
+    void testSerialization(JsonMapper jsonMapper) throws IOException {
+        var summary = SecretSummary.builder()
+            .secretName("foo")
+            .description("bar")
+            .build();
+
+        String result = jsonMapper.writeValueAsString(summary);
+        assertEquals("{\"description\":\"bar\",\"secretName\":\"foo\"}", result);
+
+        SecretSummary secretSummary = jsonMapper.readValue(result, SecretSummary.class);
+        assertNotNull(secretSummary);
+        assertEquals("foo", secretSummary.getSecretName());
+    }
+}
