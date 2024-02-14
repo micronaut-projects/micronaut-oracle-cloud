7f3578e2e7d3c6e19588b6ddcf91797ac68a952f
Sergio del Amo <sergio.delamo@softamo.com>
2024-02-14 11:16:05 +0100

    build: managed-ojdbc (#804)
    * build: managed-ojdbc
    
    * managed bom
    
    * rename to boms-oracle-jdbc
    
    * micronautBom

---
+++ oraclecloud-atp/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("io.micronaut.build.internal.oraclecloud-module")
+}
+
+dependencies {
+    api(projects.micronautOraclecloudBmcDatabase)
+    annotationProcessor(mnValidation.micronaut.validation.processor)
+    api(libs.managed.oracle.jdbc)
+    api(mn.micronaut.inject)
+    api(mnValidation.micronaut.validation)
+    runtimeOnly(libs.slf4j.jcl)
+
+    compileOnly(mnSql.micronaut.jdbc.hikari)
+    compileOnly(mnSql.micronaut.jdbc.ucp)
+    compileOnly(mnSql.micronaut.jdbc)
+
+    implementation(platform(libs.boms.oracle.jdbc))
+    implementation(libs.oracle.security.oraclepki)
+    implementation(libs.oracle.security.cert)
+    implementation(libs.oracle.security.core)
+    implementation(libs.oracle.xml.xdb)
+}
