d1d938d0bd74e70e86b55639fe92bd5de0a3a6d9
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-06-06 15:54:50 +0200

    add native test for function

---
+++ test-suite-graal-function/src/main/java/example/Function.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import io.micronaut.core.annotation.ReflectiveAccess;
+import io.micronaut.oraclecloud.core.TenancyIdProvider;
+import io.micronaut.oraclecloud.function.OciFunction;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class Function extends OciFunction {
+
+    @Inject
+    TenancyIdProvider tenantIdProvider;
+
+    @ReflectiveAccess // <1>
+    public String handleRequest() {
+        String tenancyId = tenantIdProvider.getTenancyId();
+        return "Your tenancy is: " + tenancyId;
+    }
+}
