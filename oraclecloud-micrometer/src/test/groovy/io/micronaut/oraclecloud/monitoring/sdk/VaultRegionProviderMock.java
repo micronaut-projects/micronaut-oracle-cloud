6b4f46151a732d892e6012d15628c59692c642a8
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-05 22:41:03 +0100

    fix test

--- oraclecloud-micrometer/src/test/groovy/io/micronaut/oraclecloud/monitoring/sdk/VaultRegionProviderMock.java
+++ oraclecloud-micrometer/src/test/groovy/io/micronaut/oraclecloud/monitoring/sdk/VaultRegionProviderMock.java
@@ -1,9 +1,10 @@
-package example;
+package io.micronaut.oraclecloud.monitoring.sdk;
 
 import com.oracle.bmc.Region;
 import com.oracle.bmc.auth.RegionProvider;
 import io.micronaut.context.annotation.BootstrapContextCompatible;
 import io.micronaut.context.annotation.Primary;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Replaces;
 import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
@@ -12,7 +13,7 @@ import jakarta.inject.Singleton;
 @Replaces(RegionProvider.class)
 @BootstrapContextCompatible
 @Singleton
-@Requires(property = "spec.name", value = "VaultRegionProviderTest")
+@Requires(property = "spec.name", value = "MonitorRegionProviderSpec")
 public class VaultRegionProviderMock implements RegionProvider {
     @Override
     public Region getRegion() {
