2829b825d267efb6d4a9f4fbe7967537e05380b9
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-05 19:43:50 +0100

    fix test

--- oraclecloud-vault/src/test/groovy/io/micronaut/oraclecloud/discovery/vault/OracleCloudVaultRegionProviderSpec.groovy
+++ oraclecloud-vault/src/test/groovy/io/micronaut/oraclecloud/discovery/vault/OracleCloudVaultRegionProviderSpec.groovy
@@ -1,10 +1,17 @@
 package io.micronaut.oraclecloud.discovery.vault
 
 import com.oracle.bmc.Region
+import com.oracle.bmc.auth.RegionProvider
 import com.oracle.bmc.vault.Vaults
+import com.oracle.bmc.vault.VaultsClient
+import io.micronaut.context.annotation.BootstrapContextCompatible
+import io.micronaut.context.annotation.Primary
 import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Requires
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
+import jakarta.inject.Singleton
 import spock.lang.Specification
 
 @MicronautTest
@@ -16,8 +23,26 @@ class OracleCloudVaultRegionProviderSpec extends Specification {
     @Inject
     Vaults vaults
 
-    void "test region provider"() {
-        vaults.getEndpoint() == "https://vaults.%s.oci.oraclecloud.com".formatted(Region.EU_JOVANOVAC_1.regionId)
+    void "it test region provider" () {
+        when:
+        String endpoint = vaults.getEndpoint()
+
+        then:
+        endpoint == Region.EU_JOVANOVAC_1.getEndpoint(VaultsClient.SERVICE).get()
+    }
+
+
+    @Singleton
+    @BootstrapContextCompatible
+    @Primary
+    @Replaces(RegionProvider.class)
+    @Requires(property = "spec.name", value = "OracleCloudVaultRegionProviderSpec")
+    static class RegionProviderReplacement implements RegionProvider {
+
+        @Override
+        Region getRegion() {
+            return Region.EU_JOVANOVAC_1;
+        }
     }
 
 }
