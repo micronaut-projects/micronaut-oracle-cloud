1e2eddebdca57bad6756bca756fc25cf3407d832
Sergio del Amo <sergio.delamo@softamo.com>
2024-12-11 10:07:37 +0100

    remove @Primary
    remove @Primary

--- oraclecloud-micrometer/src/test/groovy/io/micronaut/oraclecloud/monitoring/sdk/VaultRegionProviderMock.java
+++ oraclecloud-micrometer/src/test/groovy/io/micronaut/oraclecloud/monitoring/sdk/VaultRegionProviderMock.java
@@ -9,7 +9,6 @@ import io.micronaut.context.annotation.Replaces;
 import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
 
-@Primary
 @Replaces(RegionProvider.class)
 @BootstrapContextCompatible
 @Singleton
