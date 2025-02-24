792b7cfdf02e032bc431a86e0cab0feac4084484
Nemanja Mikic <nemanja.mikic@oracle.com>
2025-02-24 19:01:09 +0100

    fix tests

--- oraclecloud-httpclient-netty/src/test/java/io/micronaut/oraclecloud/httpclient/netty/MockAuthenticationDetailsProvider.java
+++ oraclecloud-httpclient-netty/src/test/java/io/micronaut/oraclecloud/httpclient/netty/MockAuthenticationDetailsProvider.java
@@ -2,10 +2,9 @@ package io.micronaut.oraclecloud.httpclient.netty;
 
 import com.oracle.bmc.auth.AuthCachingPolicy;
 import com.oracle.bmc.auth.AuthenticationDetailsProvider;
-import com.oracle.bmc.auth.BasicAuthenticationDetailsProvider;
-import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
 import io.micronaut.context.annotation.Primary;
 import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
 
 import java.io.InputStream;
@@ -14,6 +13,9 @@ import java.io.InputStream;
 @Singleton
 @Replaces(AuthenticationDetailsProvider.class)
 @Primary
+@Requires(missingProperty = "vault.secrets.compartment.ocid")
+@Requires(missingProperty = "vault.ocid")
+@Requires(missingProperty = "monitoring.compartment.ocid")
 public class MockAuthenticationDetailsProvider implements AuthenticationDetailsProvider {
 
     @Override
