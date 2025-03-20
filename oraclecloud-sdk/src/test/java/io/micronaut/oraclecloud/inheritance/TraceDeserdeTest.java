e9ce1b4c1fd3df741dcebabc12f7d853fb0d1d06
Graeme Rocher <graeme.rocher@oracle.com>
2025-03-20 15:52:34 +0100

    simplify test

--- oraclecloud-sdk/src/test/java/io/micronaut/oraclecloud/inheritance/TraceDeserdeTest.java
+++ oraclecloud-sdk/src/test/java/io/micronaut/oraclecloud/inheritance/TraceDeserdeTest.java
@@ -4,15 +4,14 @@ import com.oracle.bmc.generativeaiagentruntime.model.ChatResult;
 import com.oracle.bmc.generativeaiagentruntime.model.GenerationTrace;
 import com.oracle.bmc.generativeaiagentruntime.model.Message;
 import com.oracle.bmc.generativeaiagentruntime.model.MessageContent;
-import com.oracle.bmc.generativeaiagentruntime.model.Trace;
 import io.micronaut.json.JsonMapper;
-import io.micronaut.serde.annotation.Serdeable;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest(startApplication = false)
@@ -20,19 +19,13 @@ public class TraceDeserdeTest {
 
     @Test
     void testDeserializeInheritance(JsonMapper jsonMapper) throws IOException {
-        String result = jsonMapper.writeValueAsString(
-            new Holder(List.of())
-        );
-
-        assertNotNull(result);
         ChatResult.Builder message = ChatResult.builder()
             .traces(List.of(GenerationTrace.builder().generation("test").build()))
             .message(Message.builder().content(MessageContent.builder().text("some text").build()).build());
         String value = jsonMapper.writeValueAsString(message.build());
 
         assertNotNull(value);
+        assertEquals("{\"message\":{\"content\":{\"text\":\"some text\"}},\"traces\":[{\"traceType\":\"GENERATION_TRACE\",\"generation\":\"test\"}]}", value);
     }
 
-    @Serdeable
-    record Holder(List<Trace> trace) {}
 }
