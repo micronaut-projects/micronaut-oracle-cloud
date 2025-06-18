35971de5de9b0d358997abdc20ffc20cea156fe8
Jonas Konrad <jonas.konrad@oracle.com>
2025-06-18 12:16:13 +0200

    Use DefaultHttpClient builder (#972)
    * Use DefaultHttpClient builder
    Draft because it requires core 4.7
    
    * remove snapshot
    
    * remove snapshot
    
    * add null check
    
    * ci: projectVersion=5.2.0-SNAPSHOT [ci skip]
    
    * fix(deps): update dependency io.micronaut.discovery:micronaut-discovery-client-bom to v4.7.0 (#1129)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix(deps): update dependency io.micronaut.logging:micronaut-logging-bom to v1.7.0 (#1128)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix(deps): update dependency io.micronaut:micronaut-core-bom to v4.9.2 (#1126)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix(deps): update fn to v1.0.209 (#1125)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update dependency com.google.protobuf:protoc to v3.25.8 (#1124)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix(deps): update dependency org.apache.groovy:groovy-bom to v4.0.27 (#1123)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix(deps): update dependency io.micronaut.serde:micronaut-serde-bom to v2.15.0 (#1117)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * gradle 8.14.2
    
    * micronaut groovy 4.7.0
    
    * micronaut groovy 4.7.0
    
    * micronaut micrometer 5.11.0
    
    * reactor 3.8.0
    
    * rxjava2 2.8.0
    
    * servlet 5.4.0
    
    * sql 6.2.0
    
    * fix(deps): update oci to v3.67.2 (#1130)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * platform 4.8.3
    
    * tck: ignore CacheControlTest
    
    ---------
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- oraclecloud-oke-workload-identity/src/main/java/io/micronaut/oraclecloud/oke/workload/identity/MicronautOkeWorkloadIdentityResourcePrincipalsFederationClient.java
+++ oraclecloud-oke-workload-identity/src/main/java/io/micronaut/oraclecloud/oke/workload/identity/MicronautOkeWorkloadIdentityResourcePrincipalsFederationClient.java
@@ -29,26 +29,11 @@ import com.oracle.bmc.http.internal.ClientIdFilter;
 import com.oracle.bmc.http.internal.LogHeadersFilter;
 import com.oracle.bmc.http.signing.RequestSigner;
 import com.oracle.bmc.util.internal.StringUtils;
-import io.micronaut.buffer.netty.NettyByteBufferFactory;
-import io.micronaut.core.annotation.AnnotationMetadataResolver;
-import io.micronaut.core.convert.ConversionService;
 import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.http.MediaType;
-import io.micronaut.http.body.ContextlessMessageBodyHandlerRegistry;
 import io.micronaut.http.client.netty.DefaultHttpClient;
-import io.micronaut.http.codec.MediaTypeCodecRegistry;
-import io.micronaut.http.netty.body.NettyByteBufMessageBodyHandler;
-import io.micronaut.http.netty.body.NettyJsonHandler;
-import io.micronaut.http.netty.body.NettyJsonStreamHandler;
-import io.micronaut.http.netty.body.NettyWritableBodyWriter;
-import io.micronaut.json.JsonMapper;
-import io.micronaut.json.codec.JsonMediaTypeCodec;
-import io.micronaut.json.codec.JsonStreamMediaTypeCodec;
+import io.micronaut.http.client.netty.DefaultHttpClientBuilder;
 import io.micronaut.oraclecloud.httpclient.netty.ManagedNettyHttpProvider;
 import io.micronaut.oraclecloud.httpclient.netty.OciNettyClientFilter;
-import io.micronaut.runtime.ApplicationConfiguration;
-import io.netty.channel.MultithreadEventLoopGroup;
-import io.netty.util.concurrent.DefaultThreadFactory;
 
 import javax.net.ssl.TrustManagerFactory;
 import java.io.FileInputStream;
@@ -145,28 +130,15 @@ final class MicronautOkeWorkloadIdentityResourcePrincipalsFederationClient exten
     }
 
     DefaultHttpClient defaultHttpClient() {
-        JsonMapper mapper = JsonMapper.createDefault();
-        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
-        ContextlessMessageBodyHandlerRegistry registry = new ContextlessMessageBodyHandlerRegistry(
-            applicationConfiguration,
-            NettyByteBufferFactory.DEFAULT,
-            new NettyByteBufMessageBodyHandler(),
-            new NettyWritableBodyWriter(applicationConfiguration)
-        );
-        registry.add(MediaType.APPLICATION_JSON_TYPE, new NettyJsonHandler<>(mapper));
-        registry.add(MediaType.APPLICATION_JSON_STREAM_TYPE, new NettyJsonStreamHandler<>(mapper));
         // Set ca cert when talking to proxymux using https.
-
-        return new DefaultHttpClient(null,
-            getOkeHttpClientConfiguration(), null, new DefaultThreadFactory(MultithreadEventLoopGroup.class),
-            okeNettyClientSslBuilder(KUBERNETES_SERVICE_ACCOUNT_CERT_PATH),
-            MediaTypeCodecRegistry.of(
-                new JsonMediaTypeCodec(mapper, applicationConfiguration, null),
-                new JsonStreamMediaTypeCodec(mapper, applicationConfiguration, null)
-            ),
-            registry,
-            AnnotationMetadataResolver.DEFAULT,
-            ConversionService.SHARED);
+        DefaultHttpClientBuilder builder = DefaultHttpClient.builder();
+        OkeHttpClientConfiguration configuration = getOkeHttpClientConfiguration();
+        if (configuration != null) {
+            builder.configuration(configuration);
+        }
+        return builder
+            .nettyClientSslBuilder(okeNettyClientSslBuilder(KUBERNETES_SERVICE_ACCOUNT_CERT_PATH))
+            .build();
     }
 
     @Override
