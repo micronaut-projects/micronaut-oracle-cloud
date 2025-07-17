e1d79e31f927963ab6ae29621b96feb343c5924d
Nemanja Mikic <nemanja.mikic@oracle.com>
2025-07-17 17:02:24 +0200

    fix sonar issues

--- oraclecloud-common/src/test/java/io/micronaut/oraclecloud/core/RetryDefaultConfigTest.java
+++ oraclecloud-common/src/test/java/io/micronaut/oraclecloud/core/RetryDefaultConfigTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest
-public class RetryDefaultConfigTest {
+class RetryDefaultConfigTest {
 
     @Test
     void testRetryDefaultConfig(
