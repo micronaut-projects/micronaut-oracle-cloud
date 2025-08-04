7886a2a3d41703b8186cc190ff7da79a54f4436c
Sergio del Amo <sergio.delamo@softamo.com>
2025-08-04 10:40:17 +0200

    Revert "add publishToSonatype script [ci skip]"
    This reverts commit 786c4f32371950cdb388517cae8b2ecd0ec89730.

--- publishToSonatype.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-REPO_DIR="build/repo/io/micronaut/oraclecloud"
-for dir in "$REPO_DIR"/*/; do
-  project_name=$(basename "$dir")
-  ./gradlew ":$project_name:publishToSonatype"
-  sleep 2
-done
