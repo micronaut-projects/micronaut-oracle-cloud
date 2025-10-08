ef19daf5c536f671a63266626b5d1d28f0f237ea
Nemanja Mikic <nemanja.mikic@oracle.com>
2025-10-08 13:30:14 +0200

    gradle update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "7.6.4"
+    id("io.micronaut.build.shared.settings") version "8.0.0-M3"
     id("me.champeau.includegit") version "0.3.1"
     id("io.micronaut.build.internal.ocisdk")
 }
