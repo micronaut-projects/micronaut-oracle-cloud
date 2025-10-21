95addc968c1c785da96d783070f5655abfd2d982
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-21 08:20:50 +0200

    close after test

--- oraclecloud-httpclient-netty/src/test/java/io/micronaut/oraclecloud/httpclient/netty/ProxySystemPropertiesTest.java
+++ oraclecloud-httpclient-netty/src/test/java/io/micronaut/oraclecloud/httpclient/netty/ProxySystemPropertiesTest.java
@@ -1,11 +1,12 @@
 package io.micronaut.oraclecloud.httpclient.netty;
 
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.http.client.DefaultHttpClientConfiguration;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
 
+import java.io.Closeable;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.ProxySelector;
@@ -25,17 +26,9 @@ class ProxySystemPropertiesTest {
     private static final String P_TYPE = "io.micronaut.oci.proxy.type";
     private static final String P_NPH  = "io.micronaut.oci.proxy.nonProxyHosts";
 
-    @AfterEach
-    void cleanup() {
-        clearAll();
-    }
-
     @Test
     void configIsAppliedFromUriProperty() {
-        // given
-        clearAll();
-        System.setProperty(P_PROXY, "http://user:pass@proxy.example:8080");
-
+        SystemProperty systemProperty = new SystemProperty(P_PROXY, "http://user:pass@proxy.example:8080");
         DefaultHttpClientConfiguration cfg = new DefaultHttpClientConfiguration();
 
         // when
@@ -49,17 +42,17 @@ class ProxySystemPropertiesTest {
         assertEquals(8080, addr.getPort());
         assertEquals("user", cfg.getProxyUsername().get());
         assertEquals("pass", cfg.getProxyPassword().get());
+        systemProperty.close();
     }
 
     @Test
     void configIsAppliedFromDiscretePropertiesWithSocks() {
-        // given
-        clearAll();
-        System.setProperty(P_HOST, "socks.gateway.internal");
-        System.setProperty(P_PORT, "1081");
-        System.setProperty(P_TYPE, "socks5");
-        System.setProperty(P_USER, "alice");
-        System.setProperty(P_PASS, "secret");
+        // given:
+        SystemProperty systemPropertyHost = new SystemProperty(P_HOST, "socks.gateway.internal");
+        SystemProperty systemPropertyPort = new SystemProperty(P_PORT, "1081");
+        SystemProperty systemPropertyType = new SystemProperty(P_TYPE, "socks5");
+        SystemProperty systemPropertyUser = new SystemProperty(P_USER, "alice");
+        SystemProperty systemPropertyPass = new SystemProperty(P_PASS, "secret");
 
         DefaultHttpClientConfiguration cfg = new DefaultHttpClientConfiguration();
 
@@ -73,15 +66,21 @@ class ProxySystemPropertiesTest {
         assertEquals(1081, addr.getPort());
         assertEquals("alice", cfg.getProxyUsername().get());
         assertEquals("secret", cfg.getProxyPassword().get());
+
+        // close
+        systemPropertyHost.close();
+        systemPropertyPort.close();
+        systemPropertyType.close();
+        systemPropertyUser.close();
+        systemPropertyPass.close();
     }
 
     @Test
     void nonProxyHostsBypassesMatchingHosts() {
         // given
-        clearAll();
-        System.setProperty(P_HOST, "proxy.acme.local");
-        System.setProperty(P_PORT, "3128");
-        System.setProperty(P_NPH, "*.internal,localhost,127.0.0.1");
+        SystemProperty systemPropertyHost = new SystemProperty(P_HOST, "proxy.acme.local");
+        SystemProperty systemPropertyPort = new SystemProperty(P_PORT, "3128");
+        SystemProperty systemPropertyNph = new SystemProperty(P_NPH, "*.internal,localhost,127.0.0.1");
 
         DefaultHttpClientConfiguration cfg = new DefaultHttpClientConfiguration();
 
@@ -99,6 +98,11 @@ class ProxySystemPropertiesTest {
         List<Proxy> forExternal = selector.select(URI.create("https://service.external"));
         Assertions.assertFalse(forExternal.isEmpty());
         Assertions.assertNotEquals(Proxy.NO_PROXY, forExternal.get(0), "Non-matching hosts should use proxy");
+
+        // close
+        systemPropertyHost.close();
+        systemPropertyPort.close();
+        systemPropertyNph.close();
     }
 
     private static void clearAll() {
@@ -109,5 +113,25 @@ class ProxySystemPropertiesTest {
         System.clearProperty(P_PASS);
         System.clearProperty(P_TYPE);
         System.clearProperty(P_NPH);
+    }
+
+    static class SystemProperty implements Closeable {
+        private final String name;
+        private final String previousValue;
+
+        SystemProperty(String name, String value) {
+            this.previousValue = System.getProperty(name);
+            this.name = name;
+            System.setProperty(name, value);
+        }
+
+        @Override
+        public void close() {
+            if (StringUtils.isNotEmpty(previousValue)) {
+                System.setProperty(name, previousValue);
+            } else {
+                System.clearProperty(name);
+            }
+        }
     }
 }
