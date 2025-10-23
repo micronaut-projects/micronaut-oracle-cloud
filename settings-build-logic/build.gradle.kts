b644f2b63b2a102eb9f10ee67a4f6ec2d8a4569e
issam liqali <liqaliissam@gmail.com>
2025-10-23 15:16:27 +0100

    update to jvm 21 baseline

--- settings-build-logic/build.gradle.kts
+++ settings-build-logic/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     `kotlin-dsl`
 }
@@ -6,8 +7,13 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
     }
 }
