ff3be041a20b6e190ccbd51079bb61163c3a7cdc
Sergio del Amo <sergio.delamo@softamo.com>
2026-02-26 15:46:04 +0100

    build: set kotlin compiler options

--- settings-build-logic/build.gradle.kts
+++ settings-build-logic/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     `kotlin-dsl`
 }
@@ -12,5 +10,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_25
     targetCompatibility = JavaVersion.VERSION_25
 }
-
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25
+    }
+}
 
