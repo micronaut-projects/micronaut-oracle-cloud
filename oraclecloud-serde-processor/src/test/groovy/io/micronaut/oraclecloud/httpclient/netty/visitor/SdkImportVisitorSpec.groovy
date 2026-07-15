6f9a1c89e71aaba75eb432ed85059fce3efe2db9
Issam Liqali <liqaliissam@gmail.com>
2026-07-15 16:33:12 +0100

    Fix SdkImport model package detection (#1329)
    - Infer the SDK model package for `@SdkImport` from imported client request/response method signatures.
    - Preserve the existing `<client package>.model` fallback for standard OCI SDK clients.
    - Add a regression test for generated clients whose models are under a nested sibling package such as `...client.model`.
    
    ---------
    
    Co-authored-by: Nemanja Mikic <nemanja.mikic@oracle.com>

--- oraclecloud-serde-processor/src/test/groovy/io/micronaut/oraclecloud/httpclient/netty/visitor/SdkImportVisitorSpec.groovy
+++ oraclecloud-serde-processor/src/test/groovy/io/micronaut/oraclecloud/httpclient/netty/visitor/SdkImportVisitorSpec.groovy
@@ -24,4 +24,92 @@ class Test {}
         // check introspections are generated
         definition.beanType.classLoader.loadClass('test.$com_oracle_bmc_objectstorage_model_ReencryptObjectDetails$Introspection')
     }
+
+    void "test generate OCI SDK factory for client package model subpackage"() {
+        given:
+        def context = buildContext(new JavaFiles()
+            .add("test.Test", """
+package test;
+
+import com.oracle.health.ehrc.organizer.OrganizerClient;
+import io.micronaut.oraclecloud.core.sdk.SdkImport;
+
+@SdkImport(OrganizerClient.class)
+class Test {}
+""")
+            .add("com.oracle.health.ehrc.organizer.OrganizerClient", """
+package com.oracle.health.ehrc.organizer;
+
+import com.oracle.bmc.auth.AbstractAuthenticationDetailsProvider;
+import com.oracle.bmc.circuitbreaker.CircuitBreakerConfiguration;
+import com.oracle.bmc.common.RegionalClientBuilder;
+import com.oracle.bmc.http.internal.BaseSyncClient;
+import com.oracle.health.ehrc.organizer.client.requests.GetPractitionerRequest;
+import com.oracle.health.ehrc.organizer.client.responses.GetPractitionerResponse;
+
+public class OrganizerClient extends BaseSyncClient {
+    protected OrganizerClient(Builder builder, AbstractAuthenticationDetailsProvider authDetailsProvider) {
+        super(builder, authDetailsProvider, (CircuitBreakerConfiguration) null);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public GetPractitionerResponse getPractitioner(GetPractitionerRequest request) {
+        return null;
+    }
+
+    public static class Builder extends RegionalClientBuilder<Builder, OrganizerClient> {
+        public Builder() {
+            super(null);
+        }
+
+        @Override
+        public OrganizerClient build(AbstractAuthenticationDetailsProvider authDetailsProvider) {
+            return new OrganizerClient(this, authDetailsProvider);
+        }
+    }
+}
+""")
+            .add("com.oracle.health.ehrc.organizer.client.requests.GetPractitionerRequest", """
+package com.oracle.health.ehrc.organizer.client.requests;
+
+public class GetPractitionerRequest {
+}
+""")
+            .add("com.oracle.health.ehrc.organizer.client.responses.GetPractitionerResponse", """
+package com.oracle.health.ehrc.organizer.client.responses;
+
+public class GetPractitionerResponse {
+}
+""")
+            .add("com.oracle.health.ehrc.organizer.client.model.Practitioner", """
+package com.oracle.health.ehrc.organizer.client.model;
+
+import com.oracle.bmc.http.client.internal.ExplicitlySetBmcModel;
+
+public class Practitioner extends ExplicitlySetBmcModel {
+    private final String id;
+
+    public Practitioner(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
+"""))
+
+        when:
+        BeanDefinition definition = context.classLoader.loadClass('test.$OrganizerClientFactory$Definition').newInstance()
+
+        then:
+        definition.stringValue(SerdeImport, "packageName").get() == 'com.oracle.health.ehrc.organizer.client.model'
+        context.classLoader.loadClass('test.$com_oracle_health_ehrc_organizer_client_model_Practitioner$Introspection')
+
+        cleanup:
+        context.close()
+    }
 }
