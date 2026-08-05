1d89e8bbd894f1ff8d38953815ff9a13c57f59db
issam liqali <liqaliissam@gmail.com>
2026-08-05 15:13:36 +0100

    Fix OKE workload identity Serde metadata

--- oraclecloud-httpclient-netty/src/main/java/io/micronaut/oraclecloud/serde/OciSdkMicronautSerializer.java
+++ oraclecloud-httpclient-netty/src/main/java/io/micronaut/oraclecloud/serde/OciSdkMicronautSerializer.java
@@ -20,6 +20,8 @@ import com.oracle.bmc.auth.internal.JWK;
 import com.oracle.bmc.auth.internal.X509FederationClient;
 import com.oracle.bmc.auth.okeworkloadidentity.internal.GetOkeResourcePrincipalSessionTokenDetails;
 import com.oracle.bmc.auth.okeworkloadidentity.internal.OkeResourcePrincipalSessionToken;
+import com.oracle.bmc.auth.okeworkloadidentity.internal.contract.GetOkeResourcePrincipalSessionTokenAndKeysDetails;
+import com.oracle.bmc.auth.okeworkloadidentity.internal.contract.OkeResourcePrincipalSessionTokenAndKeys;
 import com.oracle.bmc.encryption.internal.EncryptionHeader;
 import com.oracle.bmc.encryption.internal.EncryptionKey;
 import com.oracle.bmc.http.client.Serializer;
@@ -49,6 +51,8 @@ import java.util.Map;
 @SerdeImport(X509FederationClient.X509FederationRequest.class)
 @SerdeImport(GetOkeResourcePrincipalSessionTokenDetails.class)
 @SerdeImport(OkeResourcePrincipalSessionToken.class)
+@SerdeImport(GetOkeResourcePrincipalSessionTokenAndKeysDetails.class)
+@SerdeImport(OkeResourcePrincipalSessionTokenAndKeys.class)
 @SerdeImport(value = EncryptionHeader.class, deserializable = false)
 @SerdeImport(EncryptionKey.class)
 public final class OciSdkMicronautSerializer implements Serializer {
